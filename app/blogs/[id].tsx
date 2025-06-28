49fae0336fa4b56fe4493bb11745fdf376236265
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2025-06-28 19:41:52 +0100

    Add tab navigation and initial screens for blog app

---
+++ app/blogs/[id].tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function BlogDetails() {
+  return (
+    <View>
+      <Text>BlogDetails</Text>
+    </View>
+  );
+}
