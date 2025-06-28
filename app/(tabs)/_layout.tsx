1c74e5f2feaaeb9f399afffd2ffe198001e93c17
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2025-06-28 21:08:40 +0100

    Update app name and icons; implement custom tab icons for navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,36 +2,96 @@ import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
 import React from "react";
-import { Image, ImageBackground } from "react-native";
+import { Image, ImageBackground, Text, View } from "react-native";
+
+type TabIconProps = {
+  focused: boolean;
+  icon: any;
+  title: string;
+};
+
+const TabIcon = ({ focused, icon, title }: TabIconProps) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="mt-4 flex min-h-16 w-full min-w-[90px] flex-1 flex-row items-center justify-center overflow-hidden rounded-full"
+      >
+        <Image source={icon} className="size-5" />
+        <Text className="text-secondary text-base font-semibold">{title}</Text>
+      </ImageBackground>
+    );
+  }
+  return (
+    <View className=" flex-1 items-center justify-center rounded-full">
+      <Image source={icon} className=" mt-4 size-5" />
+    </View>
+  );
+};
 
 const _Layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          height: "100%",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          height: 54,
+          borderRadius: 54,
+          marginHorizontal: 20,
+          marginBottom: 20,
+          position: "absolute",
+          overflow: "hidden",
+          backgroundColor: "#fff",
+          borderWidth: 1,
+          borderColor: "#fff",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
           headerShown: false,
           tabBarIcon: ({ focused }) => (
-            <>
-              <ImageBackground source={images.highlight}>
-                <Image source={icons.home} className="size-5" />
-              </ImageBackground>
-            </>
+            <TabIcon icon={icons.home} title="Home" focused={focused} />
           ),
         }}
       />
       <Tabs.Screen
         name="search"
-        options={{ title: "Search", headerShown: false }}
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.search} title="Search" focused={focused} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="saved"
-        options={{ title: "Saved", headerShown: false }}
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.save} title="Saved" focused={focused} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="profile"
-        options={{ title: "Profile", headerShown: false }}
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.person} title="Profile" focused={focused} />
+          ),
+        }}
       />
     </Tabs>
   );
