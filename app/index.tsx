287964ff7c0b75955533fe92fe6c336b3dc906f4
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2025-06-28 18:46:38 +0100

    Project Init with nativewind support.

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl text-cyan-500">Welcome to my blog</Text>
+    </View>
+  );
+}
